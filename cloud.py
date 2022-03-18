ce8429bff5bdf667858a9858b15a54244f6f6690
Tiago Gaspar <tiagongaspar@gmail.com>
2022-03-18 23:14:06 +0000

    stable build

---
+++ cloud.py
@@ -0,0 +1,28 @@
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import wikipedia
+import matplotlib.pyplot as py
+import streamlit as st
+
+
+def create_cloud(content):
+    #mask = np.array(Image.open("search1.png"))
+    cloud = WordCloud(mode="RGBA" , background_color="rgba(255, 255, 255, 0)", max_words = 300, stopwords = STOPWORDS, height=800, width=1600)
+    cloud.generate(content)
+    #image_colors = ImageColorGenerator(mask)
+    #cloud = cloud.recolor(color_func=image_colors)
+    return cloud
+
+def get_content(phrase):
+    
+    try:
+        page = wikipedia.page(phrase, auto_suggest=False)
+    except wikipedia.exceptions.DisambiguationError as e:
+        page = wikipedia.page(e.options[0])
+    return page.content
+    
+@st.cache(show_spinner=False)
+def get_cloud(phrase):
+    content = get_content(phrase)
+    cloud = create_cloud(content)
+    return cloud
+
---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import cloud
+import matplotlib.pyplot as py
+
+
+st.set_page_config(layout="wide", page_title="Wikipedia Wordcloud", page_icon="☁️")
+
+# page setup
+st.markdown(""" <style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
+
+st.markdown("<h1 style='text-align: center;'>Wikipedia Wordcloud Generator</h1>", unsafe_allow_html=True)
+
+phrase = st.text_input('Search Term', 'Wikipedia', help='Write here a word or phrase to be searched on Wikipedia')
+
+with st.spinner('Wait for it...'):
+    # plotting
+    wcloud = cloud.get_cloud(phrase)
+    image = py.figure(figsize=(20,10), facecolor='k')
+    py.imshow(wcloud)
+    py.axis("off")
+    py.tight_layout(pad=0)
+    st.pyplot(image)
